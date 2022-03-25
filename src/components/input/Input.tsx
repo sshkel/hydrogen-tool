18807df8f9e10b49db72bc197c8c7929c445a0a4
Stanis Shkel <stanis@canva.com>
2022-03-26 10:11:58 +1100

    format

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,28 +1,27 @@
-import Box from '@mui/material/Box';
-import InputNumberField from './InputNumberField';
-import InputExpand from './InputExpand';
-import Button from '@mui/material/Button';
-import React, { useState } from 'react';
-import { data, regionData, technologyData } from './data';
-import InputSelectField from './InputSelectField';
+import Box from "@mui/material/Box";
+import InputNumberField from "./InputNumberField";
+import InputExpand from "./InputExpand";
+import Button from "@mui/material/Button";
+import React, { useState } from "react";
+import { data, regionData, technologyData } from "./data";
+import InputSelectField from "./InputSelectField";
 
 interface Props {
   setState: (obj: any) => void;
 }
 
-
 const getDefaultState = () => {
-  const defaultState: any = {}
-  data.forEach(field => {
+  const defaultState: any = {};
+  data.forEach((field) => {
     defaultState[field.id] = field.defaultValue;
   });
-  defaultState['technology'] = technologyData[0];
-  defaultState['region'] = regionData[0];
+  defaultState["technology"] = technologyData[0];
+  defaultState["region"] = regionData[0];
   return defaultState;
-}
+};
 
-export default function Input(props: Props) {  
-  const [ formData, setFormData ] = useState(getDefaultState());
+export default function Input(props: Props) {
+  const [formData, setFormData] = useState(getDefaultState());
   let pointer: number = 0;
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>, form: any) => {
@@ -32,14 +31,25 @@ export default function Input(props: Props) {
   };
 
   const handleChange = (e: { target: { value: any; name: string } }) => {
-    setFormData({...formData, [e.target.name]: e.target.value});
-  }
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
   const getData = (index: number) => {
     ++pointer;
-    const { label, id, defaultValue, adornmentLabel, disabled, helperText } = data[index];
-    return <InputNumberField label={label} name={id} defaultValue={defaultValue} adornmentLabel={adornmentLabel} disabled={disabled} helperText={helperText} onChange={handleChange} />;
-  }
+    const { label, id, defaultValue, adornmentLabel, disabled, helperText } =
+      data[index];
+    return (
+      <InputNumberField
+        label={label}
+        name={id}
+        defaultValue={defaultValue}
+        adornmentLabel={adornmentLabel}
+        disabled={disabled}
+        helperText={helperText}
+        onChange={handleChange}
+      />
+    );
+  };
 
   return (
     <Box
@@ -48,46 +58,40 @@ export default function Input(props: Props) {
       sx={{
         width: "66%",
         height: "50%",
-        '& .MuiTextField-root': { m: 2, width: "40%" },
-        '& .selectWrapper': { m: 2, width: "40%" },
-        '& .MuiButton-root': { m: 2 },
+        "& .MuiTextField-root": { m: 2, width: "40%" },
+        "& .selectWrapper": { m: 2, width: "40%" },
+        "& .MuiButton-root": { m: 2 },
       }}
       onSubmit={(e: React.FormEvent<HTMLFormElement>) => onSubmit(e, formData)}
     >
-      <InputExpand
-        title="Scope of Analysis"
-        id="scope-of-analysis"
-      >
-        <InputSelectField id="location" label="Location" values={regionData} defaultValue={regionData[0]} onChange={handleChange} />
+      <InputExpand title="Scope of Analysis" id="scope-of-analysis">
+        <InputSelectField
+          id="location"
+          label="Location"
+          values={regionData}
+          defaultValue={regionData[0]}
+          onChange={handleChange}
+        />
       </InputExpand>
-      <InputExpand
-        title="System Sizing"
-        id="system-sizing"
-      >
-        <InputSelectField id="technology" label="Technology" values={technologyData} defaultValue={technologyData[0]} onChange={handleChange} />
-        {[...Array(7)].map((_) => (getData(pointer)))}
+      <InputExpand title="System Sizing" id="system-sizing">
+        <InputSelectField
+          id="technology"
+          label="Technology"
+          values={technologyData}
+          defaultValue={technologyData[0]}
+          onChange={handleChange}
+        />
+        {[...Array(7)].map((_) => getData(pointer))}
       </InputExpand>
-      <InputExpand
-        title="Electrolyser Costs"
-        id="electrolyser-costs"
-      >
-        {[...Array(6)].map((_) => (getData(pointer)))}
+      <InputExpand title="Electrolyser Costs" id="electrolyser-costs">
+        {[...Array(6)].map((_) => getData(pointer))}
       </InputExpand>
-      <InputExpand
-        title="Power Plant Costs"
-        id="power-plant-costs"
-      >
-        <InputExpand
-          title="Solar Costs"
-          id="solar-costs"
-        >
-        {[...Array(6)].map((_) => (getData(pointer)))}
+      <InputExpand title="Power Plant Costs" id="power-plant-costs">
+        <InputExpand title="Solar Costs" id="solar-costs">
+          {[...Array(6)].map((_) => getData(pointer))}
         </InputExpand>
-        <InputExpand
-          title="Wind Costs"
-          id="wind-costs"
-        >
-          {[...Array(6)].map((_) => (getData(pointer)))}
+        <InputExpand title="Wind Costs" id="wind-costs">
+          {[...Array(6)].map((_) => getData(pointer))}
         </InputExpand>
         <InputExpand
           title="Costs for Grid Connected Systems"
@@ -96,19 +100,12 @@ export default function Input(props: Props) {
           {getData(pointer)}
         </InputExpand>
       </InputExpand>
-      <InputExpand
-        title="Battery Costs"
-        id="battery-costs"
-      >
-        {[...Array(3)].map((_) => (getData(pointer)))}
+      <InputExpand title="Battery Costs" id="battery-costs">
+        {[...Array(3)].map((_) => getData(pointer))}
       </InputExpand>
-      <InputExpand
-        title="Additional Costs"
-        id="additional-costs"
-      >
-        {[...Array(2)].map((_) => (getData(pointer)))}
+      <InputExpand title="Additional Costs" id="additional-costs">
+        {[...Array(2)].map((_) => getData(pointer))}
       </InputExpand>
-
 
       {/* <InputExpand
         title="Electrolyser Parameters"
@@ -133,7 +130,9 @@ export default function Input(props: Props) {
           <InputNumberField label={data[14].label} name={data[14].id} defaultValue={data[14].defaultValue} adornmentLabel={data[14].adornmentLabel} disabled={data[14].disabled} helperText={data[14].helperText} onChange={handleChange} />
         </InputExpand>
       </InputExpand> */}
-      <Button variant="contained" type="submit">Calculate</Button>
+      <Button variant="contained" type="submit">
+        Calculate
+      </Button>
     </Box>
   );
 }
