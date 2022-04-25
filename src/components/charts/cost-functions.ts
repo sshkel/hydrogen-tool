7f11ed75b5cb8a63617cfe97e825d778e49ef939
Stanis Shkel <stanis@canva.com>
2022-04-25 16:05:39 +1000

    move functions

--- src/components/charts/cost-functions.ts
+++ src/components/charts/cost-functions.ts
@@ -1,4 +1,12 @@
-import { projectYears } from "../../model/Utils";
+import {
+  first,
+  decomissioning,
+  projectYears,
+  activeYears,
+  padArray,
+} from "../../model/Utils";
+import { DepreciationProfile } from "../../types";
+
 export const getBaseLog = (n: number, base: number): number =>
   Math.log(n) / Math.log(base);
 
@@ -115,4 +123,257 @@ export function cumulativeStackReplacementYears(
     }
   }
   return stackReplacementYears;
+}
+
+export function cashFlowAnalysis(
+  annualSales: number[],
+  totalOpex: number[],
+  totalCapexCost: number,
+  totalEpcCost: number,
+  totalLandCost: number,
+  shareOfTotalInvestmentFinancedViaEquity: number,
+  directEquityShare: number,
+  salvageCostShare: number,
+  decommissioningCostShare: number,
+  loanTerm: number,
+  interestOnLoan: number,
+  capitalDepreciationProfile: DepreciationProfile,
+  taxRate: number,
+  projectLife: number,
+  inflationRate: number
+) {
+  const inflation = applyInflation(inflationRate);
+  // sales
+  const salesTotal = inflation(annualSales);
+
+  // net investments
+  // direct equity payment
+
+  const totalInvestmentRequired = totalCapexCost + totalEpcCost + totalLandCost;
+  const totalEquity = roundToNearestThousand(
+    totalInvestmentRequired * shareOfTotalInvestmentFinancedViaEquity
+  );
+  const directEquityPayment = first(
+    roundToNearestThousand(totalEquity * directEquityShare),
+    projectLife
+  );
+  // indirect equity
+  const indirectEquityShare = 1 - directEquityShare;
+  // Equity supported externally (grants etc) - Indirect equity is considered as a positive cash flow
+  const indirectEquity = first(
+    roundToNearestThousand(totalEquity * indirectEquityShare),
+    projectLife
+  );
+  const shareOfTotalInvestmentFinancedViaLoan =
+    1 - shareOfTotalInvestmentFinancedViaEquity;
+  // cost financed via loan
+  const totalLoan =
+    totalInvestmentRequired * shareOfTotalInvestmentFinancedViaLoan;
+  const costFinancedViaLoan = first(totalLoan, projectLife);
+
+  // salvage cost
+  const totalSalvageCost = totalInvestmentRequired * salvageCostShare;
+  const salvageCost = decomissioning(totalSalvageCost, projectLife);
+
+  // decomissioning cost
+  const decomissioningCost = decomissioning(
+    totalInvestmentRequired * decommissioningCostShare,
+    projectLife
+  );
+
+  // cost of setting up the project
+  const netInvestment = directEquityPayment.map(
+    (_: number, i: number) =>
+      directEquityPayment[i] +
+      decomissioningCost[i] -
+      // TODO check if costFinancedViaLoan should be positive since it's money you get in and repay later every year
+      costFinancedViaLoan[i] -
+      indirectEquity[i] -
+      salvageCost[i]
+  );
+
+  // loan liabilities
+  // loan repayment
+  const loanRepayment = totalLoan / loanTerm;
+  const totalLoanRepayment = padArray(
+    projectYears(projectLife).map((year: number) => {
+      if (year < loanTerm) {
+        return loanRepayment;
+      }
+      return 0;
+    })
+  );
+  // interest paid on loan
+  const interestPaidOnLoan = padArray(
+    projectYears(projectLife).map((year: number) => {
+      if (year < loanTerm) {
+        return loanRepayment * (1 + interestOnLoan) ** year - loanRepayment;
+      }
+      return 0;
+    })
+  );
+  // fixed opex
+  const totalOpexWithInflation = inflation(padArray(totalOpex));
+  // depreciation
+  const incomePreDepreciation = directEquityPayment.map(
+    (_: number, i: number) => {
+      return (
+        salesTotal[i] -
+        netInvestment[i] -
+        totalLoanRepayment[i] -
+        interestPaidOnLoan[i] -
+        totalOpexWithInflation[i]
+      );
+    }
+  );
+  const totalDepreciableCapex = totalCapexCost + totalEpcCost;
+  const conversionFactors = getConversionFactors(
+    capitalDepreciationProfile,
+    projectLife
+  );
+  const depreciation = conversionFactors.map(
+    (_: number, i: number) => totalDepreciableCapex * conversionFactors[i]
+  );
+
+  // tax liabilities
+  const taxableIncome = incomePreDepreciation.map(
+    (_: number, i: number) =>
+      // TODO check that if totalLoanRepayment should include total interest paid
+      incomePreDepreciation[i] - depreciation[i] - totalLoanRepayment[i]
+  );
+
+  const tax = taxableIncome.map((_: number, i: number) => {
+    if (taxableIncome[i] < 0) {
+      return 0;
+    }
+    return taxableIncome[i] * taxRate;
+  });
+  // net cash flows
+  // after tax and depreciation
+  const incomeAfterTaxAndDepreciation = incomePreDepreciation.map(
+    (_: number, i: number) => incomePreDepreciation[i] - tax[i]
+  );
+  const cumulativeSum = (
+    (sum: number) => (value: number) =>
+      (sum += value)
+  )(0);
+  const cumulativeCashFlow = incomeAfterTaxAndDepreciation.map(cumulativeSum);
+
+  return {
+    directEquityPayment,
+    indirectEquity,
+    costFinancedViaLoan,
+    salvageCost,
+    decomissioningCost,
+    totalLoanRepayment,
+    interestPaidOnLoan,
+    totalOpexWithInflation,
+    incomePreDepreciation,
+    depreciation,
+    taxableIncome,
+    tax,
+    incomeAfterTaxAndDepreciation,
+    cumulativeCashFlow,
+  };
+}
+
+export function sales(
+  h2RetailPrice: number,
+  oxygenRetailPrice: number,
+  averageElectricitySpotPrice: number,
+  h2Produced: number[],
+  electricityProduced: number[],
+  electricityConsumed: number[]
+) {
+  // The values can be used to create sales graphs.
+  const h2Sales = h2Produced.map((x) => x * 1000 * h2RetailPrice);
+
+  const electricitySales = electricityProduced.map(
+    (_: number, i: number) =>
+      (electricityProduced[i] - electricityConsumed[i]) *
+      averageElectricitySpotPrice
+  );
+  const oxygenSales = h2Produced.map(
+    (_: number, i: number) => 8 * h2Produced[i] * oxygenRetailPrice
+  );
+
+  const annualSales = h2Sales.map(
+    (_: number, i: number) => h2Sales[i] + electricitySales[i] + oxygenSales[i]
+  );
+  return {
+    h2Sales,
+    electricitySales,
+    oxygenSales,
+    annualSales,
+  };
+}
+
+function applyInflation(rate: number) {
+  return (values: number[]) => {
+    return values.map(
+      // i corresponds to year
+      (x: number, i: number) => {
+        // zero-th year is always skipped as it signifies upfront costs rather than actual operations
+        if (i === 0) {
+          return x;
+        }
+        return x * (1 + rate) ** i;
+      }
+    );
+  };
+}
+
+function getConversionFactors(
+  capitalDepreciationProfile: DepreciationProfile,
+  projectLife: number
+) {
+  let selectedModel = null;
+  // come from conversion factors tab
+  // can probs use the formula instead of hardcoded table
+  switch (capitalDepreciationProfile) {
+    // TODO when default to straight line it breaks with undefined
+    case "Straight Line": {
+      selectedModel = Array(projectLife).fill(1 / projectLife);
+      break;
+    }
+    case "MACRs - 3 year Schedule": {
+      selectedModel = [0.3333, 0.4445, 0.1481, 0.0741];
+      break;
+    }
+    case "MACRs - 5 year Schedule": {
+      selectedModel = [0.2, 0.32, 0.192, 0.1152, 0.1152, 0.0576];
+      break;
+    }
+    case "MACRs - 7 year Schedule": {
+      selectedModel = [0.1429, 0.1749, 0.1249, 0.0893, 0.892, 0.893, 0.0446];
+      break;
+    }
+    case "MACRs - 10 year Schedule": {
+      selectedModel = [
+        0.1, 0.18, 0.144, 0.1152, 0.0922, 0.0737, 0.0655, 0.0655, 0.0656,
+        0.0655, 0.0328,
+      ];
+      break;
+    }
+    case "MACRs - 15 year Schedule": {
+      selectedModel = [
+        0.05, 0.095, 0.0855, 0.077, 0.0693, 0.0623, 0.059, 0.059, 0.0591, 0.059,
+        0.0591, 0.059, 0.0591, 0.059, 0.0591, 0.0295,
+      ];
+      break;
+    }
+    case "MACRs - 20 year Schedule": {
+      selectedModel = [
+        0.0375, 0.0722, 0.0668, 0.0618, 0.0571, 0.0529, 0.0489, 0.0452, 0.0446,
+        0.0446, 0.0446, 0.0446, 0.0446, 0.0446, 0.0446, 0.0446, 0.0446, 0.0446,
+        0.0446, 0.0446, 0.0223,
+      ];
+      break;
+    }
+    default: {
+      throw new Error("Unknown depreciation profile");
+    }
+  }
+  const padding = projectLife - selectedModel.length;
+  return padArray(selectedModel.concat(Array(padding).fill(0)));
 }
