2c76dee7d326896ec816c3ab1ab79f677cf8dddc
Tara <ttjandra@atlassian.com>
2022-07-10 13:37:04 +1000

    Add new const file

---
+++ src/model/consts.ts
@@ -0,0 +1,11 @@
+export const POWER_PLANT_CF = "Generator Capacity Factor";
+export const RATED_CAPACITY_TIME = "Time Electrolyser is at its Rated Capacity";
+export const TOTAL_OPERATING_TIME = "Total Time Electrolyser is Operating";
+export const ELECTROLYSER_CF = "Achieved Electrolyser Capacity Factor";
+export const ENERGY_INPUT = "Energy in to Electrolyser [MWh/yr]";
+export const ENERGY_OUTPUT = "Surplus Energy [MWh/yr]";
+export const BATTERY_OUTPUT = "Total Battery Output [MWh/yr]";
+export const HYDROGEN_OUTPUT_FIXED =
+  "Hydrogen Output for Fixed Operation [t/yr]";
+export const HYDROGEN_OUTPUT_VARIABLE =
+  "Hydrogen Output for Variable Operation [t/yr]";
