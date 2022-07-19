abb5487507dc1b78c2e608ffc879579665532489
Stanis Shkel <stanis@canva.com>
2022-07-19 21:10:17 +1000

    Add basic map

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
-import Input from "./components/input/Input";
+import { MapContainer, TileLayer } from "react-leaflet";
+
 import WorkingData from "./components/charts/WorkingData";
+import Input from "./components/input/Input";
 import { loadSolar, loadWind } from "./model/DataLoader";
 
 function App() {
@@ -8,6 +10,18 @@ function App() {
 
   return (
     <div className="App2">
+      <div id="map">
+        <MapContainer
+          center={[-32.27554173488815, 147.97835713324858]}
+          zoom={7}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+        </MapContainer>
+      </div>
       <Input setState={setState} />
       <WorkingData data={state} loadSolar={loadSolar} loadWind={loadWind} />
     </div>
