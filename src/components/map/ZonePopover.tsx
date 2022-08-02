d4f50fa5a1293605aad22fa6423d1cfe97e97117
Stanis Shkel <stanis@canva.com>
2022-08-02 22:02:27 +1000

    Cleaning up pop overs

--- src/components/map/ZonePopover.tsx
+++ src/components/map/ZonePopover.tsx
@@ -1,4 +1,15 @@
-import { Button, Grid, Paper, Popover, styled } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  Grid,
+  Paper,
+  Popover,
+  Typography,
+  styled,
+} from "@mui/material";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -6,8 +17,7 @@ const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
   padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
+  textAlign: "left",
 }));
 
 export function ZonePopover(props: any) {
@@ -18,53 +28,109 @@ export function ZonePopover(props: any) {
   };
 
   const summary = (
-    <Grid
-      container
-      role="presentation"
-      direction="column"
-      justifyContent="center"
-      alignItems="center"
-      height="60vh"
-      width="30vh"
-    >
-      <Grid item>
-        <Item>Location Summary</Item>
-      </Grid>
-      <Grid item>
-        <Item>Location </Item>
-      </Grid>
-      <Grid item>
-        <Item>Infrastructure</Item>
-      </Grid>
-      <Grid item>
+    <Card sx={{ width: "40vh", height: "60vh" }}>
+      <CardHeader title="Location summary" />
+      <CardContent>
+        <Grid
+          container
+          role="presentation"
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Grid container item justifyContent="space-between" flexWrap="nowrap">
+            <Grid item xs>
+              <Item>Location</Item>
+            </Grid>
+            <Grid item xs>
+              <Item>xxxxxxxxxxxxxx</Item>
+            </Grid>
+          </Grid>
+          <Grid container item justifyContent="space-between" flexWrap="nowrap">
+            <Grid item xs>
+              <Item>Regional Centers:</Item>
+            </Grid>
+            <Grid item xs>
+              <Item>Broken Hill, White Cliffs, Wilcannia, Buronga</Item>
+            </Grid>
+          </Grid>
+          <Grid container item justifyContent="space-between" flexWrap="nowrap">
+            <Grid item xs>
+              <Item>Solar/Wind capacity factor:</Item>
+            </Grid>
+            <Grid item xs>
+              <Item>25% 49% </Item>
+            </Grid>
+          </Grid>
+          <Grid container item justifyContent="space-between" flexWrap="nowrap">
+            <Grid item xs>
+              <Item>Main industries:</Item>
+            </Grid>
+            <Grid item xs>
+              <Item>Agriculture, mining</Item>
+            </Grid>
+          </Grid>
+          <Grid container item justifyContent="space-between" flexWrap="nowrap">
+            <Grid item xs>
+              <Item>Water availability:</Item>
+            </Grid>
+            <Grid item xs>
+              <Item>Low</Item>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Item>
+              <Typography variant="h5" component="div">
+                Infrastructure
+              </Typography>
+              <Typography variant="body2">
+                Includes existing electricity infrastructure e.g., transmission
+                lines, existing renewable energy sites, gas pipelines, major
+                highways, rail lines, airports Local Energy Generation: • Broken
+                Hill Solar Plant @53 MW • Silverton Wind Farm @200 MW Renewable
+                Energy Zone: AEMO Suggests Broken Hill can host a solar and wind
+                powered REZ
+              </Typography>
+            </Item>
+          </Grid>
+          <Grid item></Grid>
+        </Grid>
+      </CardContent>
+      <CardActions style={{ justifyContent: "center" }}>
         <Button variant="contained" onClick={() => setComponent("powerfuel")}>
           Start project design
         </Button>
-      </Grid>
-    </Grid>
+      </CardActions>
+    </Card>
   );
+
   const powerfuel = (
-    <Grid
-      container
-      role="presentation"
-      direction="column"
-      justifyContent="center"
-      alignItems="center"
-      height="60vh"
-      width="30vh"
-    >
-      <Grid item>Powerfuel pathway</Grid>
-      <Grid item margin="10px">
-        <Button variant="contained" onClick={startDesign}>
-          Hydrogen
-        </Button>
-      </Grid>
-      <Grid item margin="10px">
-        <Button variant="contained" onClick={startDesign}>
-          Ammonia
-        </Button>
-      </Grid>
-    </Grid>
+    <Card sx={{ width: "40vh", height: "60vh" }}>
+      <CardHeader title="Select powerfuel pathway" />
+      <CardContent>
+        <Grid
+          container
+          role="presentation"
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+          rowSpacing="30"
+          sx={{ width: "35vh", height: "30vh" }}
+        >
+          <Grid item>
+            <Button variant="contained" onClick={startDesign}>
+              Hydrogen: General hydrogen production cost for region
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button variant="contained" onClick={startDesign}>
+              Ammonia: Integrated hydrogen production and conversion into
+              Ammonia
+            </Button>
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
   );
 
   return (
