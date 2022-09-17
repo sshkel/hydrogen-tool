44f6f0b2f5468060809118636070edb913cbc4b9
Stanis Shkel <stanis@canva.com>
2022-09-17 20:00:08 +1000

    Update sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,13 +1,14 @@
-import MapIcon from "@mui/icons-material/Map";
+import RoomIcon from "@mui/icons-material/Room";
 import { Drawer, List, ListItem, ListItemIcon } from "@mui/material";
 import Divider from "@mui/material/Divider";
 import ListItemButton from "@mui/material/ListItemButton";
 import Toolbar from "@mui/material/Toolbar";
 import { useNavigate } from "react-router-dom";
+
 import { BLUE } from "./input/colors";
 
 export function SideBar() {
-  const drawerWidth = 60;
+  const drawerWidth = 70;
   const navigate = useNavigate();
   return (
     <Drawer
@@ -27,14 +28,22 @@ export function SideBar() {
       <Divider />
       <List>
         <ListItem disablePadding>
-          <ListItemButton onClick={() => navigate("/")}>
+          <ListItemButton
+            onClick={() => navigate("/")}
+            sx={{
+              minHeight: 48,
+              justifyContent: "center",
+              px: 3.5,
+            }}
+          >
             <ListItemIcon
               style={{
                 justifyContent: "center",
                 minWidth: 0,
               }}
             >
-              <MapIcon
+              <RoomIcon
+                fontSize="large"
                 style={{
                   color: "white",
                 }}
