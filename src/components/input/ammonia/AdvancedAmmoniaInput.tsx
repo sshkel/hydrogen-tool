1385cf3ad73192252aee72f425a3f2d5191617dc
Stanis Shkel <stanis@canva.com>
2022-10-29 23:21:53 +1100

    Fixup ammonia

--- src/components/input/ammonia/AdvancedAmmoniaInput.tsx
+++ src/components/input/ammonia/AdvancedAmmoniaInput.tsx
@@ -1,5 +1,6 @@
 import Grid from "@mui/material/Grid";
 
+import ControlledPowerPlantCard from "../ControlledPowerPlantCard";
 import InputCard from "../InputCard";
 import InputNumberField from "../InputNumberField";
 import InputSelect from "../InputSelect";
@@ -203,59 +204,7 @@ export default function AdvancedHydrogenInput() {
 
       <Grid container item flexDirection="column" rowSpacing={1} xs={4}>
         <Grid item>
-          <InputCard
-            title="Power Plant Parameters"
-            children={[
-              <InputCard
-                subtitle
-                mountOnEnter
-                key="powerCapacityConfiguration"
-                title="Power Plant Capacity"
-                children={[
-                  <InputNumberField
-                    key="renewableEnergyPlantOversizing"
-                    inputKey="renewableEnergyPlantOversizing"
-                  />,
-                  <InputNumberField
-                    key="solarToWindPercentage"
-                    inputKey="solarToWindPercentage"
-                  />,
-                ]}
-              />,
-              <InputSelect
-                key="powerPlantConfigurationSelect"
-                selectKey="powerPlantConfigurationSelect"
-                prompt="Power Plant Configuration"
-                selectClass="powerPlantConfiguration"
-                titles={["Standalone", "Grid Connected"]}
-                buttonChildren={[
-                  [],
-                  [
-                    <InputNumberField
-                      key="gridConnectionCost"
-                      inputKey="gridConnectionCost"
-                    />,
-                    <InputNumberField
-                      key="additionalTransmissionCharges"
-                      inputKey="additionalTransmissionCharges"
-                    />,
-                  ],
-                ]}
-              />,
-              <InputCard
-                subtitle
-                mountOnEnter
-                key="powerPlantEfficiency"
-                title="Power Plant Efficiency"
-                children={[
-                  <InputNumberField
-                    key="powerPlantDegradationRate"
-                    inputKey="powerPlantDegradationRate"
-                  />,
-                ]}
-              />,
-            ]}
-          />
+          <ControlledPowerPlantCard />
         </Grid>
       </Grid>
 
